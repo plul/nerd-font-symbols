1caf49dfe4c813f29a9a878fb31e26ccb11860e1
Asger Juul Brunshøj <asgerbrunshoj@gmail.com>
2024-10-01 16:25:00 +0200

    chore: use basecamp

--- flake.nix
+++ flake.nix
@@ -4,53 +4,33 @@
 
     rust-overlay.url = "github:oxalica/rust-overlay";
     rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+
+    basecamp.url = "github:plul/basecamp";
+    basecamp.inputs.nixpkgs.follows = "nixpkgs";
+    basecamp.inputs.rust-overlay.follows = "rust-overlay";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   outputs =
-    inputs@{ flake-parts, ... }:
+    inputs@{ basecamp, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" ];
 
       perSystem =
         {
-          config,
           pkgs,
-          system,
-          lib,
-          self',
           ...
         }:
+        let
+          basecampConfig = {
+            rust.enable = true;
+            rust.packages.cargo-udeps.enable = true;
+          };
+        in
         {
-          _module.args.pkgs = import inputs.nixpkgs {
-            inherit system;
-            overlays = [ (import inputs.rust-overlay) ];
-          };
-
-          packages = {
-            rust-toolchain-stable = pkgs.rust-bin.stable.latest.minimal.override {
-              extensions = [
-                "rust-src"
-                "clippy"
-                "rust-analyzer"
-              ];
-            };
-
-            rust-toolchain-nightly-minimal = pkgs.rust-bin.selectLatestNightlyWith (
-              toolchain: toolchain.minimal
-            );
-
-            rustfmt-nightly = pkgs.rust-bin.selectLatestNightlyWith (
-              toolchain: toolchain.minimal.override { extensions = [ "rustfmt" ]; }
-            );
-          };
-
-          devShells.default = pkgs.mkShell {
+          devShells.default = basecamp.mkShell pkgs basecampConfig // {
             packages = [
-              config.packages.rust-toolchain-stable
-              config.packages.rustfmt-nightly
-              pkgs.taplo
-              pkgs.nodePackages.prettier
-              pkgs.nixfmt-rfc-style
               pkgs.curl
             ];
           };
--- justfile
+++ justfile
@@ -5,92 +5,13 @@ _list:
 
 # Check project
 check:
-    just check-fmt
-    just test
-    just lint
-    just check-docs
-    just check-nix
-
-# Test project
-test:
+    bc-check
     cargo test
-
-# Lint project
-lint:
-    just lint-rust
-    just lint-toml
-
-# Lint Rust
-lint-rust:
-    cargo clippy --tests --examples -- -D warnings
-
-# Lint TOML
-lint-toml:
-    fd --extension=toml -X taplo lint
-
-# Check documentation
-check-docs:
     RUSTDOCFLAGS='-Dwarnings' cargo doc --no-deps
-
-# Check Nix
-check-nix:
-    nix flake check
-
-# Check formatting
-check-fmt:
-    just check-fmt-just
-    just check-fmt-nix
-    just check-fmt-toml
-    just check-fmt-rust
-    just check-fmt-markdown
-
-# Check formatting of justfile
-check-fmt-just:
-    just --unstable --fmt --check
-
-# Check formatting of Nix
-check-fmt-nix:
-    fd --extension=nix -X nixfmt --check
-
-# Check formatting of TOML
-check-fmt-toml:
-    fd --extension=toml -X taplo fmt --check
-
-# Check formatting of Rust
-check-fmt-rust:
-    cargo fmt -- --check
-
-# Check formatting of Markdown
-check-fmt-markdown:
-    fd --extension=md -X prettier --check
 
 # Format project
 fmt:
-    just fmt-just
-    just fmt-nix
-    just fmt-toml
-    just fmt-rust
-    just fmt-markdown
-
-# Format Justfile
-fmt-just:
-    just --unstable --fmt
-
-# Format Nix
-fmt-nix:
-    fd --extension=nix -X nixfmt
-
-# Format TOML
-fmt-toml:
-    fd --extension=toml -X taplo fmt
-
-# Format Rust
-fmt-rust:
-    cargo fmt
-
-# Format Markdown
-fmt-markdown:
-    fd --extension=md -X prettier --write
+    bc-fmt
 
 # Generate modules from specified NERD_FONT_VERSION
 update:
